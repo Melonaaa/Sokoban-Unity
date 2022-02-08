42ff7746194f2b1edcb47610f961a45dd3669396
GilJun <unity.gggg@gmail.com>
2022-02-08 09:39:23 +0900

    Upload Sokoban-Unity

---
+++ Sokoban-Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float _Speed = 10f;
+    Rigidbody PlayerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        float fallSpeed = PlayerRigidbody.velocity.y;
+
+        Vector3 velocity = new Vector3(inputX, 0, inputY);
+        velocity = velocity * _Speed;
+
+        velocity.y = fallSpeed;
+
+        PlayerRigidbody.velocity = velocity;
+    }
+}
